a3ba4fc758230f4e1dc9b67dd1dfaae48aef01c6
daicharn <work@chinokafu.jp>
2026-09-01 13:30:59 +0900

    依存関係の更新とスタイルの改善を実施

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,7 @@
+import {Hai} from 'mahjong_engine';
 import {Hais} from 'mahjong_engine';
 import {YakuContext} from 'mahjong_engine';
+import {MachiCalculator} from 'mahjong_engine';
 import {useState} from 'react';
 
 import './App.css';
@@ -28,9 +30,7 @@ type scoreRes = {
 type resType = {contextMax: YakuContext, yakuMapObj: Record<string, number>, scoreResultObj: scoreRes};
 
 async function GetCalcData(haiids: number[]){
-  
-  
-  const res = await fetch("https://mahjong-api-6ygb.onrender.com/calc", {
+  const res = await fetch("https://mahjong-api.daicharn.deno.net/calc", {
     method: "POST",
     headers: { "Content-Type": "application/json" },
     body: JSON.stringify({
@@ -39,7 +39,6 @@ async function GetCalcData(haiids: number[]){
   });
 
   const data: resType = await res.json();
-  console.log(JSON.stringify(data, null, 2));
   return data;
 }
 
@@ -47,81 +46,108 @@ function ResultView({ result }: { result: resType | undefined }){
   if(!result) return null;
   if(!result.yakuMapObj) return (<div><p>役が成立していません</p></div>);
   return (
-    <div>
-      <h2>役</h2>
-      <p>{result.scoreResultObj.han}翻</p>
-      <ul>
-        {Object.entries(result.yakuMapObj).map(([name, han], index) => (
-          <li key={index}>{name} ({han}翻)</li>
-        ))}
-      </ul>
-      <h2>符</h2>
-      <p>{result.scoreResultObj.fuCeiled}符({result.scoreResultObj.fuBasic})</p>
-      <ul>
-        {result.scoreResultObj.fuDetail.map((value, index) => (
-          <li key={index}>{value.name} {value.fu}符 {value.minHaiId}</li>
-        ))}
-      </ul>
-      <h2>点数</h2>
-      <p>親ロン: {result.scoreResultObj.tensuu.ronOya}</p>
-      <p>子ロン: {result.scoreResultObj.tensuu.ronKo}</p>
-      <p>親ツモ: {result.scoreResultObj.tensuu.tsumoOya}オール</p>
-      <p>子ツモ: 親{result.scoreResultObj.tensuu.tsumoKo.oya} / 子{result.scoreResultObj.tensuu.tsumoKo.ko}</p>
+    <div className='result_views'>
+      <div className='result_view'>
+        <h2>役</h2>
+        <p>{result.scoreResultObj.han}翻</p>
+        <ul>
+          {Object.entries(result.yakuMapObj).map(([name, han], index) => (
+            <li key={index}>{name} ({han}翻)</li>
+          ))}
+        </ul>
+      </div>
+      <div className='result_view'>
+        <h2>符</h2>
+        <p>{result.scoreResultObj.fuCeiled}符({result.scoreResultObj.fuBasic})</p>
+        <ul>
+          {result.scoreResultObj.fuDetail.map((value, index) => (
+            <li key={index}>{value.name} {value.fu}符 {value.minHaiId}</li>
+          ))}
+        </ul>
+      </div>
+      <div className='result_view'>
+        <h2>点数</h2>
+        <p>親ロン: {result.scoreResultObj.tensuu.ronOya}</p>
+        <p>子ロン: {result.scoreResultObj.tensuu.ronKo}</p>
+        <p>親ツモ: {result.scoreResultObj.tensuu.tsumoOya}オール</p>
+        <p>子ツモ: 親{result.scoreResultObj.tensuu.tsumoKo.oya} / 子{result.scoreResultObj.tensuu.tsumoKo.ko}</p>
+      </div>
     </div>
   );
 }
 
-function HaisView(hais: Hais){
+function HaisView({ hais }: { hais: Hais}){
   return (
-    <div className="hais">
-      {hais.getHais().map((h, i) => (
-      <div className="hai">
-      <img src={"images/" + h.imageUrl} key={i}></img>
-      <p className="hai_text">{h.getId()}</p>
+    <div>
+      {hais.length !== 0 && <h2>手牌</h2>}
+      <div className="hais">
+        {hais.getHais().map((h, i) => (
+        <div className="hai" key={i}>
+        <img src={"images/" + h.imageUrl}></img>
+        <p className="hai_text">{h.getId()}</p>
+        </div>
+        ))}
       </div>
-      ))}
+    </div>
+  );
+}
+
+function MachiHaisView({ machiHais }: { machiHais: Hai[]}){
+  return (
+    <div>
+      {machiHais.length !== 0 && <h2>待ち牌</h2>}
+      <div className="hais_machi">
+        {machiHais.map((h, i) => (
+        <div className="hai_machi" key={i}>
+        <img src={"images/" + h.imageUrl} key={i}></img>
+        </div>
+        ))}
+      </div>
     </div>
   );
 }
 
 function App() {
   const [hais, setHaiIds] = useState<Hais>(new Hais());
+  const [machiHais, setmachiHais] = useState<Hai[]>([]);
   const [result, setResult] = useState<resType>();
+  const [loading, setLoading] = useState(false);
 
-  const addHai = async (id: number) => {
-    hais.push(id);
-    hais.sort();
-    setHaiIds(new Hais(hais.ids));
+  const updateHais = async (fn: (h: Hais) => void) => {
+    const newHais = new Hais(hais.ids);
+    fn(newHais);
+    newHais.sort();
+    setHaiIds(newHais);
 
-    if(hais.ids.length === 14){
-      const data = await GetCalcData(hais.ids);
+    if(newHais.length === 14){
+      setLoading(true);
+
+      const data = await GetCalcData(newHais.ids);
       setResult(data);
+
+      setLoading(false);
     }
     else{
       setResult(undefined);
     }
-  }
-  const removeHai = async (id: number) => {
-    hais.remove(id);
-    hais.sort();
-    setHaiIds(new Hais(hais.ids));
 
-    if(hais.ids.length === 14){
-      const data = await GetCalcData(hais.ids);
-      setResult(data);
+    if(newHais.length === 13){
+      //4枚の時の処理を追加予定
+      const machiHais = new MachiCalculator(newHais.getHais()).calculate().map(m => new Hai(m));
+      setmachiHais(machiHais);
     }
     else{
-      setResult(undefined);
+      setmachiHais([]);
     }
   }
+
+  const addHai = (id: number) => updateHais(h => h.push(id));
+  const removeHai = (id: number) => updateHais(h => h.remove(id));
 
   return (
     <div className="App">
       <h1>麻雀点数計算テスト</h1>
-      <h2>手牌</h2>
       
-      {HaisView(hais)}
-
       <input id="hai_input" type="number"></input>
       <button onClick={() => {
         const haiInput = document.getElementById("hai_input") as HTMLInputElement;
@@ -138,6 +164,9 @@ function App() {
         削除
       </button>
       
+      <HaisView hais={hais} />
+      <MachiHaisView machiHais={machiHais} />
+      {loading && <div><div className="loader"></div><p className='loader_text'>表示までしばらくお待ちください...</p></div>}
       <ResultView result={result} />
       
     </div>
