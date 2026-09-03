69f2b456fcd97c407970333636465460157ffe67
daicharn <work@chinokafu.jp>
2026-09-03 10:58:36 +0900

    TehaiViewコンポーネントの構造を整理し、不要なdivを削除

--- src/components/TehaiView.tsx
+++ src/components/TehaiView.tsx
@@ -4,20 +4,18 @@ import {TILE} from 'mahjong_engine';
 
 export default function TehaiView({ hais, onRemoveHai }: { hais: Hais, onRemoveHai: (id: number) => void }){
   return (
-    <div>
-      <div className="hais">
-        {hais.getHais().map((h, i) => (
-        <div className="hai" key={i}>
-          <img src={"images/" + h.imageUrl} onClick={() => onRemoveHai(h.getId())}></img>
-        </div>
-        ))}
-        {Array(14 - hais.length).fill(0).map((_, i) => (
-        <div className="hai" key={i}>
-          <img src={"images/" + new Hai(TILE.BACK).imageUrl}></img>
-        </div>
-        ))
-        }
+    <div className="hais">
+      {hais.getHais().map((h, i) => (
+      <div className="hai" key={i}>
+        <img src={"images/" + h.imageUrl} onClick={() => onRemoveHai(h.getId())}></img>
       </div>
+      ))}
+      {Array(14 - hais.length).fill(0).map((_, i) => (
+      <div className="hai" key={i}>
+        <img src={"images/" + new Hai(TILE.BACK).imageUrl}></img>
+      </div>
+      ))
+      }
     </div>
   );
 }
