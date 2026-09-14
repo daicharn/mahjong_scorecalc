76eac8d5f72f7f9b158c794055114e189d114855
daicharn <work@chinokafu.jp>
2026-09-14 10:23:16 +0900

    next.config.tsのbasePathとassetPrefixを環境に応じて動的に設定

--- next.config.ts
+++ next.config.ts
@@ -4,10 +4,12 @@ const nextConfig: NextConfig = {
   /* config options here */
 };
 
+const isProd = process.env.NODE_ENV === 'production';
+
 module.exports = {
   output: 'export',
-  basePath: '/product/mahjongScoreCalc',
-  assetPrefix: '/product/mahjongScoreCalc',
+  basePath: isProd ? '/product/mahjongScoreCalc' : '',
+  assetPrefix: isProd ? '/product/mahjongScoreCalc' : '',
 };
 
 export default nextConfig;
